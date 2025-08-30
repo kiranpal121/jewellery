30be0a2f3775ade85b01deee8f95dbaf3c99fdc7
kiranpal121 <kiran121pal@gmail.com>
2025-08-30 20:34:47 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import ProductPage from "./pages/ProductPage";
 import HeroSection from "./components/Hero";
+import Header from "./components/header";
 
 function App() {
   return (
